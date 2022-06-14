9b6d65c1b7e0dcc74fa8327d65d1ba28fbfc2ff4
gh7 <gcoelho730@gmail.com>
2022-06-14 11:26:19 -0300

    adicionando modulo de angular - conceitos fundamentais em uma aplicação

---
+++ Angular/course-manager/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from 'app/nav-bar/nav-bar.component';
+import { ReplacePipe } from 'app/pipe/replace.pipe';
+
+import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { Error404Component } from './error404/error-404.component';
+import { StarComponent } from './star/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    CourseInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path:'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
